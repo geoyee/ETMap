6d2c0da4898392e7e28cd3683d3ad84ed7e55916
geoyee <geoyee@yeah.net>
2022-05-20 10:41:44 +0800

    Update OCR

--- utils/ocr.py
+++ utils/ocr.py
@@ -33,22 +33,40 @@ class Recognizer(object):
 
 class OCRer(object):
     def __init__(self) -> None:
+        self.det_shape = (640, 640)
         self.detor = Detector()
+        self.rec_shape = (32, 320)
         self.recter = Recognizer()
 
     def predict(self, image_file: str) -> List:
         image = cv2.imread(image_file)
-        re_image = cv2.resize(image, dsize=(640, 640), interpolation=cv2.INTER_BITS)
+        re_image = cv2.resize(image, dsize=self.det_shape, interpolation=cv2.INTER_BITS)
+        RH, RW = np.array(image.shape[:2]) / np.array(self.det_shape)
         bboxs = self.detor.predict(re_image)
         blocks = []
         for bbox in bboxs:
-            p1 = (int(bbox[0][0]), int(bbox[0][1]))
-            p2 = (int(bbox[2][0]), int(bbox[2][1]))
-            block = re_image[p1[1] : p2[1], p1[0] : p2[0], :]
-            block = cv2.resize(block, dsize=(320, 32), interpolation=cv2.INTER_BITS)
-            blocks.append(block)
-        output = self.recter.predict(blocks)
-        return output
+            p1 = (int(bbox[0][0] * RH) - 5, int(bbox[0][1] * RW) - 5)
+            p2 = (int(bbox[2][0] * RH) + 5, int(bbox[2][1] * RW) + 5)
+            block = image[p1[1] : p2[1], p1[0] : p2[0], :]
+            blocks.append(self._reblock(block))
+            cv2.rectangle(image, p1, p2, color=[0, 0, 255])
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return self._splicing(self.recter.predict(blocks))
+
+    def _reblock(self, block: np.ndarray) -> np.ndarray:
+        RH = self.rec_shape[0] / block.shape[0]
+        block = cv2.resize(
+            block, dsize=None, fx=RH, fy=RH, interpolation=cv2.INTER_BITS
+        )
+        return block
+
+    def _splicing(self, outputs: List) -> str:
+        text = ""
+        for i in range(len(outputs) - 1, -1, -1):
+            text += outputs[i][0]
+        return text
 
 
 if __name__ == "__main__":
