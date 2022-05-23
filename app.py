d97919e08006d533f04c1e063f99c401e1b7319e
geoyee <geoyee@yeah.net>
2022-05-23 22:38:26 +0800

    refactor(models): Clean

--- app.py
+++ app.py
@@ -3,54 +3,12 @@ import os.path as osp
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from utils import GeoExtracter
+from models import District, Point
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
-
-
-class District(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(10))
-
-    def __init__(self, id: int, time: str) -> None:
-        self.id = id
-        self.time = time
-
-
-class Point(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(10))
-    address = db.Column(db.String(80))
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    district_id = db.Column(db.Integer, db.ForeignKey("district.id"))
-    district = db.relationship("District")
-
-    def __init__(
-        self,
-        id: int,
-        district: District,
-        time: str,
-        address: str,
-        lat: float,
-        lng: float,
-    ) -> None:
-        self.id = id
-        self.district = district
-        self.time = time
-        self.address = address
-        self.latitude = lat
-        self.longitude = lng
-
-    def __repr__(self) -> str:
-        return "<Point %d: Add. %s Lat %s Lng %s>" % (
-            self.id,
-            self.address,
-            self.latitude,
-            self.longitude,
-        )
 
 
 @app.route("/")
@@ -86,7 +44,7 @@ def add_datas(path: str, city: str, db: SQLAlchemy) -> None:
 
 if __name__ == "__main__":
     if not osp.exists("test.db"):
-        path = "datas/test.png"
+        path = "data/test.png"
         db.create_all()
         add_datas(path, "沈阳市", db)
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,44 @@
+from app import db
+
+
+class District(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(10))
+
+    def __init__(self, id: int, time: str) -> None:
+        self.id = id
+        self.time = time
+
+
+class Point(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(10))
+    address = db.Column(db.String(80))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    district_id = db.Column(db.Integer, db.ForeignKey("district.id"))
+    district = db.relationship("District")
+
+    def __init__(
+        self,
+        id: int,
+        district: District,
+        time: str,
+        address: str,
+        lat: float,
+        lng: float,
+    ) -> None:
+        self.id = id
+        self.district = district
+        self.time = time
+        self.address = address
+        self.latitude = lat
+        self.longitude = lng
+
+    def __repr__(self) -> str:
+        return "<Point %d: Add. %s Lat %s Lng %s>" % (
+            self.id,
+            self.address,
+            self.latitude,
+            self.longitude,
+        )
